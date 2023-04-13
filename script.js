d3534702cd2b7e282e6fd662e706ccca1cb2e26a
koding88 <duongngocanh2k03@gmail.com>
2023-04-13 10:44:16 +0700

    Create about page

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-// Coundown timer
+// Header
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY >= header.offsetTop) {
+        header.classList.add("fixed");
+    } else {
+        header.classList.remove("fixed");
+    }
+    if (window.scrollY === 0) {
+        header.classList.remove("fixed");
+    }
+});
+
+// Slick slider
+$(".autoplay").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+});
+
+$(".autoplay-1").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+});
+
+// / Coundown timer
 
 const countdown = document.querySelector("[data-countdown]");
 const targetDate = new Date(countdown.dataset.countdown).getTime();
@@ -24,20 +53,3 @@ setInterval(() => {
         .toString()
         .padStart(2, "0");
 }, 1000);
-
-// Slick slider
-$(".autoplay").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
-});
-
-$(".autoplay-1").slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
-});
-
-
